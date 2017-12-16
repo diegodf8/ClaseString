81d5fd136c1c8992667bd0762016f6b5d9a0b90b
diegodf8 <diegodf8@gmail.com>
2017-12-16 14:43:30 +0100

    Subida a

--- src/com/cice/ejercicios/Ejercicio01.java
+++ src/com/cice/ejercicios/Ejercicio01.java
@@ -8,13 +8,16 @@ package com.cice.ejercicios;
 public class Ejercicio01 {
     public static void main(String[] args ){
 
-        String[] cadenas= {"esto es","aun mas largo y es la mas larga larga","menos largo","quien sabe lo que es"};
+        String[] cadenas= {"esto es","aun ","menos largo","quien sabe lo que es"};
+        String[] aux;
         int longitud = 0;
         String cadena = "";
 
-        for (int i = 0; i < cadenas.length;i++){
 
-            if (cadenas[i].length()>longitud){
+        for (int i = 0; i < cadenas.length; i++){
+
+
+            if (cadenas[i].length()>=longitud){
                 longitud = cadenas[i].length();
                 cadena = cadenas[i];
             }
@@ -22,6 +25,9 @@ public class Ejercicio01 {
 
         System.out.println(longitud);
         System.out.println(cadena);
+
+        aux= cadenas[3].split(" ");
+        System.out.println(aux[3]);
 
     }
 }
